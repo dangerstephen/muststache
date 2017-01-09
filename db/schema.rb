686a64ee96041955c99c9de12586401f9bac479c
ladydangerdame <turbofemme@gmail.com>
2017-01-09 13:45:37 -0800

    Fixing model

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109204306) do
+ActiveRecord::Schema.define(version: 20170109003054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "storages", force: :cascade do |t|
-    t.string   "location"
-    t.string   "size"
-    t.boolean  "available"
-    t.string   "type"
-    t.text     "description"
-    t.float    "price"
-    t.string   "title"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_storages_on_user_id", using: :btree
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -54,5 +40,4 @@ ActiveRecord::Schema.define(version: 20170109204306) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "storages", "users"
 end
