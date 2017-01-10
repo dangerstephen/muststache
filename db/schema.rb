f96c4d2bb2fa7c31318ffceb573cf9505b7b964a
Kevin Tran <kevin2005tran@gmail.com>
2017-01-10 10:31:30 -0800

    removed price column from spaces table to reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110045816) do
+ActiveRecord::Schema.define(version: 20170110182942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170110045816) do
     t.integer  "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "price"
     t.index ["space_id"], name: "index_reservations_on_space_id", using: :btree
     t.index ["user_id"], name: "index_reservations_on_user_id", using: :btree
   end
@@ -61,7 +62,6 @@ ActiveRecord::Schema.define(version: 20170110045816) do
     t.boolean  "available"
     t.string   "space_type"
     t.text     "description"
-    t.float    "price"
     t.string   "title"
     t.integer  "user_id"
     t.datetime "created_at",  null: false
