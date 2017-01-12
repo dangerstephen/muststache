2847f77673de62e327c76b9027536342e23c9b3c
Sol1323 <sbeniquez084@eap.edu>
2017-01-12 02:15:14 -0800

    rechecked

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,7 +4,7 @@ class ReservationsController < ApplicationController
   def preload
     space = Space.find(params[:space_id])
     today = Date.today
-    reservations = space.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+    reservations = space.reservations.where(["start_date >= ? or end_date >= ?", today, today])
 
     render json: reservations
   end
@@ -68,12 +68,12 @@ class ReservationsController < ApplicationController
   private
   def is_conflict(start_date, end_date)
     space = Space.find(params[:space_id])
-    check = space.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+    check = space.reservations.where("? < start_date and end_date < ?", start_date, end_date)
     check.size > 0? true : false
   end
 
   def reservation_params
-    params.require(:reservation).permit(:start_date, :end_date, :total, :space_id)
+    params.require(:reservation).permit(:start_date, :end_date, :price, :total, :space_id)
   end
 
 end
